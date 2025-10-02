43d8e3a420501b31a7cae4b9e644b49edbe5727b
Riefandi001 <rdsputra45@gmail.com>
2025-10-02 20:12:35 +0700

    Belajar timeling

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,48 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "home",
+    ]);
 });
 
 Route::get('/profile', function () {
-    return view('profile');
+    return view('profile',[
+        "title" => "profile",
+        "nama" => "markonah",
+        "nohp" => "00857326329",
+        "foto" => "img/amazing.png",
+    ]);
 });
 
 Route::get('/news', function () {
-    return view('news');
+
+    $data_news = [
+        [
+            "judul" => "newss 1",
+            "penulis" => "anjo",
+            "konten" => "pembunuhan tanpa busana daerah ciliwangi",
+        ],
+        [
+            "judul" => "newss 2",
+            "penulis" => "jamno",
+            "konten" => "menangis meratapi nasib, seorang pemuda ditemukan sedang melahirkan disekitar lampu merah ciliwung",
+        ],
+        [
+            "judul" => "newss 3",
+            "penulis" => "pukime",
+            "konten" => "munaroh menikami",
+        ],
+    ];
+    return view('news', [
+        "title" => "news",
+        "newss" => $data_news,
+    ]);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', [
+        "title" => "contact",
+    ]);
 });
+
